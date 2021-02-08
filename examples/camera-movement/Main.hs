876a5cb7e0a0a30ae2f5dc306797d448dcdd03cf
Jordan Medlock <jordanemedlock@gmail.com>
2021-02-07 21:13:40 -0800

    Finished camera rotation plut 3d movement

--- examples/camera-movement/Main.hs
+++ examples/camera-movement/Main.hs
@@ -90,8 +90,8 @@ void main()
     vec4 specular = specularStrength * spec * lightColor;
 
     // result
-    vec4 result = (ambient + diffuse + specular) * texture(objectTexture, TexCoord);
-    // vec4 result = texture(objectTexture, TexCoord);
+    // vec4 result = (ambient + diffuse + specular) * texture(objectTexture, TexCoord);
+    vec4 result = texture(objectTexture, TexCoord);
     FragColor = result;
 }
 |]
@@ -274,6 +274,7 @@ createPlane = do
     return (vao, indicesSize `div` 3)
 
 
+initAssets :: IO Assets
 initAssets = do
     Just program <- compileShader vert frag Nothing 
 
@@ -284,15 +285,16 @@ initAssets = do
 
     return $ Assets program (EarthAssets sphereVAO numTris tex)
 
+
+initState :: IO GameState
 initState = do
     startTime <- getCurrentTime
 
-    let cameraTransform = Transform (EulerRotation $ V3 0 0 0) $ V3 2 2 2
-    let cameraState = Camera cameraTransform 70 (16.0/9.0) 0.1 100.0
+    let cameraState = Camera 0 0 (V3 2 2 2) 70 (16.0/9.0) 0.1 100.0
 
     let earthState = Earth $ Transform (EulerRotation $ V3 (-90) 0 0) $ V3 0 0 0
 
-    return $ GameState cameraState earthState startTime
+    return $ GameState cameraState earthState startTime Nothing Playing
 
 projection :: Camera -> M44 Float
 projection cam = perspective (cam^.fov) (cam^.ratio) (cam^.near) (cam^.far)
@@ -304,6 +306,34 @@ mkRotationMat (EulerRotation (V3 x y z)) = fromQuaternion $ qx * qy * qz
         qx = axisAngle (V3 1 0 0) (degToRad x)
         qy = axisAngle (V3 0 1 0) (degToRad y)
         qz = axisAngle (V3 0 0 1) (degToRad z)
+
+transformationMatrix :: Transform Float -> M44 Float
+transformationMatrix trans = mkTransformationMat (mkRotationMat $ trans^.rotation) $ trans^.position
+
+cameraMatrix :: Camera -> M44 Float
+cameraMatrix cam = lookAt (cam^.position) (cam^.position + cameraForward cam) (V3 0 1 0)
+
+cameraForward :: Camera -> V3 Float
+cameraForward cam = normalize $ V3 dx dy dz
+    where 
+        dx = cos (degToRad $ cam^.yaw) * cos (degToRad $ cam^.pitch)
+        dy = sin (degToRad $ cam^.pitch)
+        dz = sin (degToRad $ cam^.yaw) * cos (degToRad $ cam^.pitch)
+
+cameraRight :: Camera -> V3 Float
+cameraRight cam = normalize $ cross (cameraForward cam) (V3 0 1 0)
+
+cameraUp :: Camera -> V3 Float
+cameraUp cam = normalize $ cross (cameraRight cam) (cameraForward cam)
+
+moveCameraForward :: Float -> Camera -> Camera
+moveCameraForward speed cam = cam & position +~ cameraForward cam ^* speed
+
+moveCameraRight :: Float -> Camera -> Camera
+moveCameraRight speed cam = cam & position +~ cameraRight cam ^* speed
+
+moveCameraUp :: Float -> Camera -> Camera
+moveCameraUp speed cam = cam & position +~ cameraUp cam ^* speed
 
 degToRad :: Float -> Float 
 degToRad = (*pi).(/180)
@@ -326,17 +356,62 @@ main = do
 
     let game = Game window assets state
 
+    GLFW.setCursorInputMode window GLFW.CursorInputMode'Disabled
+    GLFW.setKeyCallback window $ Just $ keyCallback game
+    GLFW.setCursorPosCallback window $ Just $ cursorPosCallback game
+
+
     runReaderT mainLoop game
 
     GLFW.terminate
+
+keyCallback :: Game -> GLFW.Window -> GLFW.Key -> Int -> GLFW.KeyState -> GLFW.ModifierKeys -> IO ()
+keyCallback game win GLFW.Key'Escape i keyState modifiers = modifyIORef (game^.gameState) $ mode .~ Stopped
+
+keyCallback game win GLFW.Key'W i keyState modifiers = modifyIORef (game^.gameState) $ camera %~ moveCameraForward (0.01)
+keyCallback game win GLFW.Key'A i keyState modifiers = modifyIORef (game^.gameState) $ camera %~ moveCameraRight (-0.01)
+keyCallback game win GLFW.Key'S i keyState modifiers = modifyIORef (game^.gameState) $ camera %~ moveCameraForward (-0.01)
+keyCallback game win GLFW.Key'D i keyState modifiers = modifyIORef (game^.gameState) $ camera %~ moveCameraRight (0.01)
+keyCallback game win GLFW.Key'Q i keyState modifiers = modifyIORef (game^.gameState) $ camera %~ moveCameraUp (0.01)
+keyCallback game win GLFW.Key'E i keyState modifiers = modifyIORef (game^.gameState) $ camera %~ moveCameraUp (-0.01)
+
+keyCallback game win key i keyState modifiers = do
+    return ()
+
+clamp :: (Ord a) => a -> a -> a -> a
+clamp min max x | x < min = min
+                | x > max = max
+                | otherwise = x
+
+cursorPosCallback :: Game -> GLFW.Window -> Double -> Double -> IO ()
+cursorPosCallback game win x y = do
+    lastPos <- maybe (V2 x y) id . view lastCursorPos <$> readIORef (game^.gameState)
+    -- print lastPos
+    let cameraSensitivity = 0.1
+    let dx = realToFrac $ (x - (lastPos^._x)) * cameraSensitivity
+    let dy = realToFrac $ ((lastPos^._y) - y) * cameraSensitivity
+    -- putStrLn $ show (V2 x y) <> " - " <> show lastPos <> " => " <> show (V2 dx dy)
+    modifyIORef (game^.gameState) $ ((camera.pitch) %~ (clamp (-89) 89).(+dy))
+                                  . ((camera.yaw) +~ dx)
+                                  . (lastCursorPos .~ Just (V2 x y))
 
 mainLoop :: ReaderT Game IO ()
 mainLoop = do
     game <- ask
     state <- liftIO $ readIORef $ game^.gameState
+    case state^.mode of
+        Playing -> playing
+        Paused -> playing 
+        Stopped -> return ()
+
+playing :: ReaderT Game IO ()
+playing = do
+    game <- ask
+    state <- liftIO $ readIORef $ game^.gameState
     let earthA = game^.assets^.earth
     let earthT = state^.earth^.transform
-
+    let cam = state^.camera
+    
     thisTime <- liftIO do
         GLFW.pollEvents
 
@@ -346,27 +421,31 @@ mainLoop = do
         getCurrentTime
     let deltaTime = diffUTCTime thisTime (state^.lastTime)
     let dt = (realToFrac deltaTime :: Double)
-    let speed = 30
+    let speed = 0
 
     liftIO do
-        modifyIORef (game^.gameState) $ (earth.transform.rotation.vector._z) %~ (+(realToFrac $ dt*speed)) 
+        modifyIORef (game^.gameState) $ (earth.transform.rotation.euler._z) %~ (+(realToFrac $ dt*speed)) 
         modifyIORef (game^.gameState) $ (lastTime) .~ thisTime
         
+    -- liftIO $ putStrLn $ "yaw: " ++ (show $ cam^.yaw) ++ " pitch: " ++ (show $ cam^.pitch)
 
-    let model = mkTransformationMat (mkRotationMat $ earthT^.rotation) $ earthT^.position
+    let view = cameraMatrix cam
 
-    let pos = V3 (2) (2) (2) :: V3 Float
-    
-    let view = lookAt pos (V3 0 0 0) (V3 0 1 0) :: M44 Float
+
+    let model = transformationMatrix earthT
+    -- let view = mkTransformationMat (mkRotationMat $ cameraT^.rotation) $ cameraT^.position
+    -- let pos = V3 (2) (2) (2) :: V3 Float
+
+    -- let view = lookAt pos (V3 0 0 0) (V3 0 1 0) :: M44 Float
 
     withShader (game^.assets^.shader) do
-        "model"         $== model
+        "model"         $== model -- TODO: add a uniform instance for Tranform
         "view"          $== view
-        "projection"    $== projection (state^.camera)
+        "projection"    $== projection cam
 
         "objectColor"   $== (color 1.0 0.5 0.3 1.0)
         "ambientColor"  $== (color 1.0 1.0 1.0 1.0)
-        "viewPos"       $== pos
+        "viewPos"       $== (cam^.position)
         "lightPos"      $== (V3 1.1 1.0 2.0 :: V3 Float)
         "lightColor"    $== (color 1.0 1.0 1.0 1.0)
 
