e1e036a63fd9748cab9f1d34bd70a4871174b0cc
Jordan Medlock <jordan@catalysis.com>
2021-05-03 20:40:48 -0700

    Fixed a few examples

--- examples/blank-window/Main.hs
+++ examples/blank-window/Main.hs
@@ -1,36 +1,10 @@
 
 module Main where
 
-import qualified Graphics.UI.GLFW as GLFW
-import qualified Graphics.Rendering.OpenGL as GL
-import Graphics.Rendering.OpenGL (($=))
 import Engine
 import Linear
 
-windowSize :: V2 Int
-windowSize = V2 1920 1080
-
-windowTitle :: String
-windowTitle = "Triangle example"
-
 main :: IO ()
 main = do
-    window <- initWindow windowSize windowTitle
-    mainLoop window
-
-    GLFW.terminate
-
-mainLoop :: GLFW.Window -> IO ()
-mainLoop window = do
-    GLFW.pollEvents
-
-    GL.clearColor $= GL.Color4 0 0 0 1
-    GL.clear [GL.ColorBuffer, GL.DepthBuffer]
-
-    -- render
-
-    GLFW.swapBuffers window
-    shouldClose <- GLFW.windowShouldClose window
-    if shouldClose 
-        then return ()
-        else mainLoop window
+    win <- initWindow (V2 800 600) "Blank window"
+    playIO () win (\_ -> return ()) (const return) (const return)
--- examples/box/Main.hs
+++ examples/box/Main.hs
@@ -1,20 +1,9 @@
 
 module Main where
 
-import qualified Graphics.UI.GLFW as GLFW
-import qualified Graphics.Rendering.OpenGL as GL
-import Graphics.Rendering.OpenGL (($=))
-import Foreign.Marshal.Array
-import Foreign.Ptr
-import Foreign.Storable
-import Data.Time (getCurrentTime, diffUTCTime, UTCTime)
 import Engine
-import Text.RawString.QQ
-import Control.Monad.IO.Class
-import Data.ByteString (ByteString)
 import Linear
-import Control.Lens
-
+import qualified Graphics.Rendering.OpenGL as GL
 
 data GameState = GameState 
     { shader :: GL.Program
@@ -28,53 +17,28 @@ main :: IO ()
 main = do
     window <- initWindow (V2 1920 1080) "Box example"
 
-    Right program <- loadShader "resources/shaders/simple" 
-
-    let ratio = windowSize^._x.to fromIntegral / windowSize^._y.to fromIntegral :: Float
-
-    projection <- perspectiveMatrix 45 ratio 0.1 100.0
-
-    withShader program $ "projection" $== projection
-
+    program <- loadFromName "simple"
     cube <- createCubeAsset
 
-    startTime <- getCurrentTime
+    playIO (GameState program cube) window draw (const return) (const return)
 
-    mainLoop window startTime (GameState program cube)
-
-    GLFW.terminate
-
-mainLoop :: GLFW.Window -> UTCTime -> GameState -> IO ()
-mainLoop window previousTime (GameState program cube) = do
-    GLFW.pollEvents
-
-    GL.clearColor $= GL.Color4 0 0 0 1
-    GL.clear [GL.ColorBuffer, GL.DepthBuffer]
-
-    thisTime <- getCurrentTime
-    let deltaTime = diffUTCTime thisTime previousTime
-    let dt = (realToFrac deltaTime :: Double)
-
-    model <- eye4 :: IO (GL.GLmatrix Float)
+draw :: GameState -> IO ()
+draw (GameState program cube) = do
     
-    view <- lookAtMatrix (GL.Vector3 1 1 1) (GL.Vector3 0 0 0) (GL.Vector3 0 1 0) :: IO (GL.GLmatrix Float)
+    let view = lookAt (V3 1 1 1) (V3 0 0 0) (V3 0 1 0 :: V3 Float)
+    let ratio = 1920 / 1080 :: Float
+    projection <- perspectiveMatrix 45 ratio 0.1 100.0
 
     withShader program do
-        "model"         $== model
+        "projection"    $== projection
+        "model"         $== (identity :: M44 Float)
         "view"          $== view
 
-        "objectColor"   $== (GL.Color4 1.0 0.5 0.3 1.0 :: GL.Color4 Float)
-        "ambientColor"  $== (GL.Color4 1.0 1.0 1.0 1.0 :: GL.Color4 Float)
-        "viewPos"       $== (GL.Vector3 1 1 1 :: GL.Vector3 Float)
-        "lightPos"      $== (GL.Vector3 1.1 1.0 2.0 :: GL.Vector3 Float)
-        "lightColor"    $== (GL.Color4 1.0 1.0 1.0 1.0 :: GL.Color4 Float)
+        "objectColor"   $== (V4 1.0 0.5 0.3 1.0 :: V4 Float)
+        "ambientColor"  $== (V4 1.0 1.0 1.0 1.0 :: V4 Float)
+        "viewPos"       $== (V3 1 1 1 :: V3 Float)
+        "lightPos"      $== (V3 1.1 1.0 2.0 :: V3 Float)
+        "lightColor"    $== (V4 1.0 1.0 1.0 1.0 :: V4 Float)
 
         drawMeshAsset cube
-        
-
-    GLFW.swapBuffers window
-    shouldClose <- GLFW.windowShouldClose window
-    if shouldClose 
-        then return ()
-        else mainLoop window thisTime (GameState program cube)
 
